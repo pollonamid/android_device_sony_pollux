fac501ab4acadcb1e03dd0bc3f795e5b20d3df25
MightyBear007@gmail.com <mightybear007@gmail.com>
2013-10-24 16:05:30 +0200

    Change Product name

--- omni_pollux.mk
+++ omni_pollux.mk
@@ -39,5 +39,5 @@ $(call inherit-product, device/sony/pollux/device.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321_1273-0364/SGP321:4.2.2/10.3.1.A.0.244/C_93rg:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.2.2 10.3.1.A.0.244 C_93rg test-keys"
 
-PRODUCT_NAME := custom_pollux
+PRODUCT_NAME := omni_pollux
 PRODUCT_DEVICE := pollux
